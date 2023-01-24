5d2df3c8df0124310bd7e277a74748773ea0da18
Wolaci <wolacijose8@gmail.com>
2023-01-24 13:38:53 -0300

    Verificando tráfego das vias

--- js/map.js
+++ js/map.js
@@ -1,13 +1,33 @@
-var map;
-var button = document.querySelector('#satellite');
-button.addEventListener('click', function(){
-    map.setMapTypeId('satellite');
-});
-function initMap(){
-    var mapOptions = {
-        center: {lat: -7.89920736, lng: -34.89892423},
-        zoom: 8,
-        mapTypeId: 'roadmap' //roadmap, satellite, hybrid, terrain
-    };
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
+function initMap() {
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer({
+        draggable: false
+    });
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 13,
+      center: { lat: -8.06350027, lng: -34.87366855 },
+    });
+    directionsRenderer.setMap(map);
+    directionsService.route({
+        origin: 'Marco zero, Recife',
+        destination: 'Abreu e Lima',
+        waypoints: [
+            // {location: 'Recife, Brasil'}
+            {location: 'Recife, Brasil', stopover: true}
+        ],
+        travelMode: google.maps.TravelMode.DRIVING
+    }).then(response => {
+        console.log({response});
+        directionsRenderer.setDirections(response);
+
+        // map.fitBounds(response.routes[0].bounds);
+        // renderPolyline(response.routes[0].overview_path, map);
+    }).catch(err => {
+        console.log({err});
+    });
+    const trafficLayer = new google.maps.TrafficLayer();
+  
+    trafficLayer.setMap(map);
+  }
+  
+  window.initMap = initMap;
